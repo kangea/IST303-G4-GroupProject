6e912d66df4b2abd498d1b0876414247e22473f1
kangea <eunice.kang.58@gmail.com>
2019-03-16 09:26:41 -0700

    created virtualenv

---
+++ virtenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\eunice\documents\github\ist303-g4-groupproject\virtenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
