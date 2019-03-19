17f64f8d162a0ed9bb9a688c9654c71f03ea845b
kangea <eunice.kang.58@gmail.com>
2019-03-18 23:11:17 -0700

    minor changes to classes, made migrations, and very basic template for homepage

--- products_monitor/models.py
+++ products_monitor/models.py
@@ -1,20 +1,34 @@
 from django.db import models
+import datetime
 
 class Brand(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     url = models.URLField()
     logo_url = models.URLField()
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     SKU = models.CharField(max_length=200)
     stock_status = models.BinaryField()
     url = models.URLField()
     picture_url = models.URLField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
+    restock_date = models.DateTimeField('date restocked')
+
+    def __str__(self):
+        return self.name
 
     def getSKU(self):
         return self.SKU
 
     def getImage(self):
         return self.picture_url
+
+    def was_restocked_recently(self):
+        now = timezone.now()
+        # check with Quan if this is a good time range
+        return now-datetime.timedelta(hour=8) <= self.restock_date <= now
--- products_monitor/views.py
+++ products_monitor/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hi. This is a test.")
+from .models import Brand, Product
+
+class IndexView(generic.ListView):
+    template_name = 'products_monitor/index.html'
+    context_objet_name = 'brand_list'
+
+    def get_queryset(self):
+        return Brand.objects.all().order_by('name')
