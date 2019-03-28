0ab7ebb9a53990c8db47155d01a4b45e9d959740
kangea <eunice.kang.58@gmail.com>
2019-03-27 20:01:07 -0700

    register brand and product to admin page

--- products_monitor/admin.py
+++ products_monitor/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Product, ProductURL
+
+class ProductURLInLine(admin.TabularInline):
+    model = ProductURL
+    extra = 2
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name','brand','SKU','instock','picture_url','price',
+        'restock_date','original_release_date']
+    inlines = [ProductURLInLine]
+
+admin.site.register(Brand)
+admin.site.register(Product, ProductAdmin)
