995694be83f1d45d91451e2672938998f1879f56
kangea <eunice.kang.58@gmail.com>
2019-05-07 18:05:48 -0700

    testing brands page

--- products_monitor/tests/products_monitor/test_pages.py
+++ products_monitor/tests/products_monitor/test_pages.py
@@ -10,21 +10,31 @@ HOMEPAGE = 'http://127.0.0.1:8000/'
 USER_NAME = 'testuser'
 USER_PASSWORD = 'quest1234'
 
-def test_loggedout_homepage(driver):
+def test_homepage(driver):
     driver.get(HOMEPAGE)
     expected = "QuEST"
     assert driver.title == expected
 
-def test_loggedin_dashboard(driver):
+def test_logginginout(driver):
     driver.get(HOMEPAGE)
     driver.find_element_by_id('loginlink').click()
     driver.find_element_by_name('username').send_keys(USER_NAME)
     driver.find_element_by_name('password').send_keys(USER_PASSWORD + Keys.RETURN)
-    driver.find_element_by_id('logoutlink')
+    driver.find_element_by_id('logoutlink').click()
+    expected = "You have logged out."
+    p = driver.find_element_by_tag_name('p')
+    assert p.text == expected
 
 def test_products(driver):
     driver.get(HOMEPAGE)
     driver.find_element_by_id('productlink').click()
     expected = "PRODUCTS"
+    h2 = driver.find_element_by_tag_name('h2')
+    assert h2.text == expected
+
+def test_brands(driver):
+    driver.get(HOMEPAGE)
+    driver.find_element_by_id('brandlink').click()
+    expected = "FEATURED BRANDS"
     h2 = driver.find_element_by_tag_name('h2')
     assert h2.text == expected
